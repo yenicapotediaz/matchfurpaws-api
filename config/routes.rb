905b57991abad0357279bc471d9f9c32acfea354
Yeni <yenicapote08@gmail.com>
2017-01-10 12:20:38 -0800

    Added a nested route for shelters to find all their pets.

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   resources :pets, except: [:new, :edit]
   resources :shelters, except: [:new, :edit]
   resources :adopters, except: [:new, :edit]
+
+  resources :shelters, except: [:new, :edit] do
+    resources :pets, except: [:new, :edit]
+  end
 end
